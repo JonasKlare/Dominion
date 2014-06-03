87f174542c8234673b0904f1eecbe46321d6adde
JonasKlare <jonasklare4@aol.com>
2014-06-03 10:31:23 -0500

    Changes
    Here is change. I am moo

---
+++ src/Environment.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+
+public class Environment 
+{
+
+	public static void main(String[] args) 
+	{
+		/*card(int cardCostNew, String cardNameNew, int cardTypeNew, String cardAbilityNew, int turnNew, int buyNew, int moneyNew, int drawNew, int cardNumber, boolean defenseNew, int pointsNew)
+		{
+			cardCost = cardCostNew;
+			cardName = cardNameNew;
+			cardType = cardTypeNew;
+			cardAbility = cardAbilityNew;
+			turn = turnNew;
+			buy = buyNew;
+			money = moneyNew;
+			draw = drawNew;	
+			defense = defenseNew;
+			points = pointsNew; 
+			i s i s i B i
+		} */
+		
+		card copper = new card(0, "Copper" , 2 , "" , 0 , 0 , 1 , 0 , 0 , false , 0);
+		card silver = new card(3, "Silver", 2 , "" , 0 , 0 , 2 , 0 , 0 , false , 0);
+		card gold = new card(6 , "Gold" , 2 , "" , 0 , 0 , 3 , 0 , 0 , false , 0);
+		
+		
+
+	}
+
+}
